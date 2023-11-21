e639478527a63b67999565cce31c289855fa68e1
Ben Wilson <g0tmi1k@kali.org>
2023-11-21 10:51:33 +0000

    Check user input with gnuplot wrapper

--- check-availability/plot.sh
+++ check-availability/plot.sh
@@ -7,6 +7,16 @@ if ! command -v gnuplot >/dev/null 2>&1; then
     exit 1
 fi
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [analyze-json.data]" >&2
+    exit 1
+fi
+
+if [ ! -e "$1" ]; then
+    echo "ERROR: Can't find $1" >&2
+    exit 1
+fi
+
 filename=$(basename ${1%.dat})
 gnuplot -c plot-total.plg $1 $filename
 gnuplot -c plot-selected-excluded.plg $1 $filename
